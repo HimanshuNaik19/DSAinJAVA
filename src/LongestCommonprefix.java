f444c48ce91f1db1a99153523f14bd3dd0dfd1b7
HimanshuNaik19 <naikhimanshu235@gmail.com>
2025-07-19 18:04:54 +0530

    new codes

--- src/LongestCommonprefix.java
+++ src/LongestCommonprefix.java
@@ -1,5 +1,17 @@
 public class LongestCommonprefix {
     public String CommonPrefix(String[] s){
-        
+        if (s == null || s.length == 0) {
+            return "";
+        }
+        String pre= s[0];
+        for(int i=0;i<=s.length ;i++){
+            while(s[i].indexOf(pre)!=0){
+                pre = pre.substring(0,pre.length()-1);
+                if(pre.isEmpty()){
+                    return "";
+                }
+            }
+        }
+        return pre;
     }
 }
